2f829cfb789d9bfc39c1be91a73d3c79e9260487
febc.yamamoto <yamamoto.febc@gmail.com>
2016-08-09 13:42:25 +0900

    Refactoring source location and packages

--- builtin/bins/provider-sakuracloud/main.go
+++ builtin/bins/provider-sakuracloud/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	//"github.com/hashicorp/terraform/builtin/providers/sakuracloud"
 	"github.com/hashicorp/terraform/plugin"
-	sakuracloud "github.com/yamamoto-febc/terraform-provider-sakuracloud"
+	"github.com/yamamoto-febc/terraform-provider-sakuracloud/builtin/providers/sakuracloud"
 )
 
 func main() {
