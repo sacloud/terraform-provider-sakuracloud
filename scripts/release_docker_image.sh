ab9839933657eee19c1805b6a9caad303b5273de
febc.yamamoto <yamamoto.febc@gmail.com>
2017-11-12 23:43:45 +0900

    Build docker-image when running CI/CD

--- scripts/release_docker_image.sh
+++ scripts/release_docker_image.sh
@@ -8,7 +8,7 @@ cd terraform-for-sakuracloud-docker
 git fetch origin
 
 # check version
-CURRENT_VERSION=`git tag | perl -ne 'if(/([0-9\.]+)$/){print $1;exit}'`
+CURRENT_VERSION=`git tag -l --sort=-v:refname | perl -ne 'if(/^v([0-9\.]+)$/){print $1;exit}'`
 if [ "$CURRENT_VERSION" = "$VERSION" ] ; then
     echo "v$VERSION is already released."
     exit 0
