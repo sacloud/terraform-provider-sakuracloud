0d5bf3fcb43b7bea6d7af4a0aba021a6b84220b8
febc.yamamoto <yamamoto.febc@gmail.com>
2019-12-05 13:11:50 +0900

    Add -mod=vendor

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,6 +15,7 @@ for GOOS in $OS; do
         GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=0 \
             go build \
                 -ldflags "$BUILD_LDFLAGS" \
+                -mod=vendor \
                 -o bin/$binary \
                 main.go
         if [ -n "$ARCHIVE" ]; then
