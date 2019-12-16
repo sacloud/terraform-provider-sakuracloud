d3df71684de6a7d6b66f0a1ceef2d192ad68f299
febc.yamamoto <yamamoto.febc@gmail.com>
2019-12-16 17:50:51 +0900

    Make uploading via FTPS cancelable

---
+++ sakuracloud/ftps_client.go
@@ -0,0 +1,41 @@
+package sakuracloud
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/sacloud/ftps"
+)
+
+func uploadFileViaFTPS(ctx context.Context, user, pass, host, file string) error {
+
+	f, err := os.Open(file)
+	if err != nil {
+		return fmt.Errorf("opening file[%s] failed: %s", file, err)
+	}
+	defer f.Close() // nolint
+
+	compCh := make(chan struct{})
+	errCh := make(chan error)
+	defer close(compCh)
+	defer close(errCh)
+
+	ftpClient := ftps.NewClient(user, pass, host)
+	go func() {
+		if err := ftpClient.UploadFile(filepath.Base(file), f); err != nil {
+			errCh <- err
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		f.Close() // nolint
+		return ctx.Err()
+	case err := <-errCh:
+		return err
+	case <-compCh:
+		return nil
+	}
+}
