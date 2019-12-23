3d12cbc5636ccbeb365c866dbe905906bb606bdd
febc.yamamoto <yamamoto.febc@gmail.com>
2019-12-23 21:05:16 +0900

    Container Registry

---
+++ sakuracloud/resource_sakuracloud_container_registry.go
@@ -0,0 +1,192 @@
+// Copyright 2016-2019 terraform-provider-sakuracloud authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sakuracloud
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/sacloud/libsacloud/v2/sacloud"
+	"github.com/sacloud/libsacloud/v2/sacloud/types"
+)
+
+func resourceSakuraCloudContainerRegistry() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSakuraCloudContainerRegistryCreate,
+		Read:   resourceSakuraCloudContainerRegistryRead,
+		Update: resourceSakuraCloudContainerRegistryUpdate,
+		Delete: resourceSakuraCloudContainerRegistryDelete,
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Read:   schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"visibility": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringInSlice(types.ContainerRegistryVisibilityStrings, false),
+			},
+			"prefix": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"fqdn": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"icon_id": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validateSakuracloudIDType,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"tags": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Set:      schema.HashString,
+			},
+			"user": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"password": {
+							Type:      schema.TypeString,
+							Required:  true,
+							Sensitive: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceSakuraCloudContainerRegistryCreate(d *schema.ResourceData, meta interface{}) error {
+	client, _, err := sakuraCloudClient(d, meta)
+	if err != nil {
+		return err
+	}
+	ctx, cancel := operationContext(d, schema.TimeoutCreate)
+	defer cancel()
+
+	builder := expandContainerRegistryBuilder(d, client, "")
+	reg, err := builder.Build(ctx)
+	if err != nil {
+		return fmt.Errorf("creating SakuraCloud ContainerRegistry is failed: %s", err)
+	}
+
+	d.SetId(reg.ID.String())
+	return resourceSakuraCloudContainerRegistryRead(d, meta)
+}
+
+func resourceSakuraCloudContainerRegistryRead(d *schema.ResourceData, meta interface{}) error {
+	client, _, err := sakuraCloudClient(d, meta)
+	if err != nil {
+		return err
+	}
+	ctx, cancel := operationContext(d, schema.TimeoutRead)
+	defer cancel()
+
+	regOp := sacloud.NewContainerRegistryOp(client)
+
+	reg, err := regOp.Read(ctx, sakuraCloudID(d.Id()))
+	if err != nil {
+		if sacloud.IsNotFoundError(err) {
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("could not find SakuraCloud ContainerRegistry[%s]: %s", d.Id(), err)
+	}
+	return setContainerRegistryResourceData(ctx, d, client, reg)
+}
+
+func resourceSakuraCloudContainerRegistryUpdate(d *schema.ResourceData, meta interface{}) error {
+	client, _, err := sakuraCloudClient(d, meta)
+	if err != nil {
+		return err
+	}
+	ctx, cancel := operationContext(d, schema.TimeoutUpdate)
+	defer cancel()
+
+	regOp := sacloud.NewContainerRegistryOp(client)
+	reg, err := regOp.Read(ctx, sakuraCloudID(d.Id()))
+	if err != nil {
+		return fmt.Errorf("could not read SakuraCloud ContainerRegistry[%s]: %s", d.Id(), err)
+	}
+
+	builder := expandContainerRegistryBuilder(d, client, reg.SettingsHash)
+	if _, err := builder.Update(ctx, reg.ID); err != nil {
+		return fmt.Errorf("updating SakuraCloud ContainerRegistry[%s] is failed: %s", reg.ID, err)
+	}
+
+	return resourceSakuraCloudContainerRegistryRead(d, meta)
+}
+
+func resourceSakuraCloudContainerRegistryDelete(d *schema.ResourceData, meta interface{}) error {
+	client, _, err := sakuraCloudClient(d, meta)
+	if err != nil {
+		return err
+	}
+	ctx, cancel := operationContext(d, schema.TimeoutDelete)
+	defer cancel()
+
+	regOp := sacloud.NewContainerRegistryOp(client)
+	reg, err := regOp.Read(ctx, sakuraCloudID(d.Id()))
+	if err != nil {
+		if sacloud.IsNotFoundError(err) {
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("could not read SakuraCloud ContainerRegistry[%s]: %s", d.Id(), err)
+	}
+
+	if err := regOp.Delete(ctx, reg.ID); err != nil {
+		return fmt.Errorf("deleting SakuraCloud ContainerRegistry[%s] is failed: %s", reg.ID, err)
+	}
+
+	d.SetId("")
+	return nil
+}
+
+func setContainerRegistryResourceData(ctx context.Context, d *schema.ResourceData, client *APIClient, data *sacloud.ContainerRegistry) error {
+	d.Set("name", data.Name)               // nolint
+	d.Set("visibility", data.Visibility)   // nolint
+	d.Set("prefix", data.NamePrefix)       // nolint
+	d.Set("fqdn", data.FQDN)               // nolint
+	d.Set("icon_id", data.IconID.String()) // nolint
+	d.Set("description", data.Description) // nolint
+	return d.Set("tags", flattenTags(data.Tags))
+}
