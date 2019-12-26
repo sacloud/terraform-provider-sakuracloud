254742963a75e0f4ab58b867632692767805cf27
febc.yamamoto <yamamoto.febc@gmail.com>
2019-12-26 09:31:50 +0900

    Fix some - add lisence-header - fix broken test

--- tools/tfdocgen/schema_test.go
+++ tools/tfdocgen/schema_test.go
@@ -1,3 +1,17 @@
+// Copyright 2016-2019 terraform-provider-sakuracloud authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package tfdocgen
 
 import (
@@ -81,22 +95,17 @@ func TestNewSchema(t *testing.T) {
 				},
 			},
 		},
-		"id": {
-			Type:     schema.TypeString,
-			Computed: true,
-		},
 	}
 
 	expect := &Schema{
 		Arguments: []Argument{
 			{Name: "arg1", Optional: true},
-			{Name: "nest1_arg", Optional: true, Description: "One or more `nest1_arg` blocks as defined below."},
-			{Name: "nest1_attr", Optional: true, Description: "One or more `nest1_attr` blocks as defined below."},
-			{Name: "nest1_both", Optional: true, Description: "A `nest1_both` block as defined below."},
+			{Name: "nest1_arg", Optional: true, Description: "One or more `nest1_arg` blocks as defined below"},
+			{Name: "nest1_attr", Optional: true, Description: "One or more `nest1_attr` blocks as defined below"},
+			{Name: "nest1_both", Optional: true, Description: "A `nest1_both` block as defined below"},
 			{Name: "slice1", Optional: true},
 		},
 		Attributes: []Attribute{
-			{Name: "id"},
 			{Name: "attr1"},
 		},
 		ArgumentBlocks: []ArgumentBlock{
@@ -112,7 +121,7 @@ func TestNewSchema(t *testing.T) {
 				Parents: []string{"nest1_both"},
 				Arguments: []Argument{
 					{Name: "arg2", Optional: true},
-					{Name: "nest2", Optional: true, Description: "One or more `nest2` blocks as defined below."},
+					{Name: "nest2", Optional: true, Description: "One or more `nest2` blocks as defined below"},
 				},
 			},
 			{
