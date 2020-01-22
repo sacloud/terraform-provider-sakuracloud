74fbee56c51337795d3337708e53e30001419cd5
febc.yamamoto <yamamoto.febc@gmail.com>
2020-01-22 13:56:06 +0900

    Update docs - LocalRouter

--- examples/r/database_read_replica.tf
+++ examples/r/database_read_replica.tf
@@ -1,9 +1,9 @@
 resource "sakuracloud_database_read_replica" "foobar" {
-  master_id    = data.sakuracloud_database.master.id
-  ip_address   = "192.168.11.111"
-  name         = "foobar"
-  description  = "description"
-  tags         = ["tag1", "tag2"]
+  master_id   = data.sakuracloud_database.master.id
+  ip_address  = "192.168.11.111"
+  name        = "foobar"
+  description = "description"
+  tags        = ["tag1", "tag2"]
 }
 
 data sakuracloud_database "master" {
