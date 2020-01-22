0e584fa6d8b82fab53092ec7e78158faba7e06c3
febc.yamamoto <yamamoto.febc@gmail.com>
2020-01-22 13:35:06 +0900

    Supports sakuracloud_local_router resource/dataSource

---
+++ sakuracloud/data_source_sakuracloud_local_router_test.go
@@ -0,0 +1,89 @@
+// Copyright 2016-2020 terraform-provider-sakuracloud authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sakuracloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccSakuraCloudDataSourceLocalRouter_basic(t *testing.T) {
+	resourceName := "data.sakuracloud_local_router.foobar"
+	rand := randomName()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: buildConfigWithArgs(testAccSakuraCloudDataSourceLocalRouter_basic, rand),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckSakuraCloudDataSourceExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", rand),
+					resource.TestCheckResourceAttr(resourceName, "description", "description"),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "3"),
+					resource.TestCheckResourceAttr(resourceName, "tags.4151227546", "tag1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.1852302624", "tag2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.425776566", "tag3"),
+					resource.TestCheckResourceAttrPair(
+						resourceName, "switch.0.code",
+						"sakuracloud_switch.foobar", "id"),
+					resource.TestCheckResourceAttr(resourceName, "switch.0.category", "cloud"),
+					resource.TestCheckResourceAttrPair(
+						resourceName, "switch.0.zone_id",
+						"data.sakuracloud_zone.current", "name"),
+					resource.TestCheckResourceAttr(resourceName, "network_interface.0.vip", "192.168.11.1"),
+					resource.TestCheckResourceAttr(resourceName, "network_interface.0.ip_addresses.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "network_interface.0.ip_addresses.0", "192.168.11.11"),
+					resource.TestCheckResourceAttr(resourceName, "network_interface.0.ip_addresses.1", "192.168.11.12"),
+					resource.TestCheckResourceAttr(resourceName, "network_interface.0.netmask", "24"),
+					resource.TestCheckResourceAttr(resourceName, "network_interface.0.vrid", "1"),
+				),
+			},
+		},
+	})
+}
+
+var testAccSakuraCloudDataSourceLocalRouter_basic = `
+resource sakuracloud_switch "foobar" {
+  name = "{{ .arg0 }}"
+}
+
+data sakuracloud_zone "current" {}
+
+resource "sakuracloud_local_router" "foobar" {
+  switch {
+    code     = sakuracloud_switch.foobar.id
+    category = "cloud"
+    zone_id  = data.sakuracloud_zone.current.name
+  }
+  network_interface {
+    vip          = "192.168.11.1"
+    ip_addresses = ["192.168.11.11", "192.168.11.12"]
+    netmask      = 24
+    vrid         = 1
+  }
+
+  name        = "{{ .arg0 }}"
+  description = "description"
+  tags        = ["tag1", "tag2", "tag3"]
+}
+
+data "sakuracloud_local_router" "foobar" {
+  filter {
+	names = [sakuracloud_local_router.foobar.name]
+  }
+}`
