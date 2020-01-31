94191c9ff0c8fb1a44a6afaf3cd87e644f8484f8
febc.yamamoto <yamamoto.febc@gmail.com>
2020-01-31 11:25:52 +0900

    Bump to v2.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
-## 2.0.0 (Unreleased)
+## 2.0.1 (Unreleased)
+
+*
+
+## 2.0.0 (2020-01-31)
 
 NOTES:
 
-* Initial release of v2.x 
+* Initial release of v2.0
 
--- sakuracloud/version.go
+++ sakuracloud/version.go
@@ -18,7 +18,7 @@ import "fmt"
 
 var (
 	// Version app version
-	Version = "2.0.0-rc7"
+	Version = "2.0.0"
 	// Revision git commit short commithash
 	Revision = "xxxxxx" // set on build
 )
