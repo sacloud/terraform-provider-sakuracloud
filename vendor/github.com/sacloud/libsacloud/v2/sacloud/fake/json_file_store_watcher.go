2a95b33768571dd459c335225acbad949fc1406f
febc.yamamoto <yamamoto.febc@gmail.com>
2020-02-26 12:00:50 +0900

    Use libsacloud v2.1.4

--- vendor/github.com/sacloud/libsacloud/v2/sacloud/fake/json_file_store_watcher.go
+++ vendor/github.com/sacloud/libsacloud/v2/sacloud/fake/json_file_store_watcher.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build freebsd openbsd netbsd dragonfly darwin windows
+// +build !wasm
 
 package fake
 
--- vendor/github.com/sacloud/libsacloud/v2/version.go
+++ vendor/github.com/sacloud/libsacloud/v2/version.go
@@ -15,4 +15,4 @@
 package libsacloud
 
 // Version バージョン
-const Version = "2.1.3"
+const Version = "2.1.4"
