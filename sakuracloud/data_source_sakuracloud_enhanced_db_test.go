7fbb2e8b73103ad8bf1caa672d55b5e06aa63ab7
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2021-07-08 15:40:25 +0900

    Enhanced Database

---
+++ sakuracloud/data_source_sakuracloud_enhanced_db_test.go
@@ -0,0 +1,65 @@
+// Copyright 2016-2021 terraform-provider-sakuracloud authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sakuracloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccSakuraCloudDataSourceEnhancedDB_basic(t *testing.T) {
+	resourceName := "data.sakuracloud_enhanced_db.foobar"
+	rand := randomName()
+	databaseName := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+	password := randomPassword()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: buildConfigWithArgs(testAccSakuraCloudDataSourceEnhancedDB_basic, rand, databaseName, password),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckSakuraCloudDataSourceExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", rand),
+					resource.TestCheckResourceAttr(resourceName, "description", "description"),
+					resource.TestCheckResourceAttr(resourceName, "database_name", databaseName),
+					resource.TestCheckResourceAttr(resourceName, "database_type", "tidb"),
+					resource.TestCheckResourceAttr(resourceName, "region", "is1"),
+					resource.TestCheckResourceAttr(resourceName, "max_connections", "50"),
+					resource.TestCheckResourceAttr(resourceName, "host_name", databaseName+".tidb-is1.db.sakurausercontent.com"),
+				),
+			},
+		},
+	})
+}
+
+var testAccSakuraCloudDataSourceEnhancedDB_basic = `
+resource "sakuracloud_enhanced_db" "foobar" {
+  name            = "{{ .arg0 }}"
+  database_name   = "{{ .arg1 }}"
+  password        = "{{ .arg2 }}"
+
+  description = "description"
+  tags        = ["tag1", "tag2"]
+}
+
+data "sakuracloud_enhanced_db" "foobar" {
+  filter {
+    names = [sakuracloud_enhanced_db.foobar.name]
+  }
+}`
---
+++ sakuracloud/resource_sakuracloud_enhanced_db.go
@@ -0,0 +1,175 @@
+// Copyright 2016-2021 terraform-provider-sakuracloud authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sakuracloud
+
+import (
+	"context"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/sacloud/libsacloud/v2/sacloud"
+)
+
+func resourceSakuraCloudEnhancedDB() *schema.Resource {
+	resourceName := "Enhanced Database"
+	return &schema.Resource{
+		CreateContext: resourceSakuraCloudEnhancedDBCreate,
+		ReadContext:   resourceSakuraCloudEnhancedDBRead,
+		UpdateContext: resourceSakuraCloudEnhancedDBUpdate,
+		DeleteContext: resourceSakuraCloudEnhancedDBDelete,
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": schemaResourceName(resourceName),
+			"database_name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "The name of database",
+			},
+			"password": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Sensitive:   true,
+				Description: "The password of database",
+			},
+			"region": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The region name",
+			},
+			"database_type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The type of database",
+			},
+			"host_name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The name of database host. This will be built from `database_name` + `tidb-is1.db.sakurausercontent.com`",
+			},
+			"max_connections": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "The value of max connections setting",
+			},
+			"icon_id":     schemaResourceIconID(resourceName),
+			"description": schemaResourceDescription(resourceName),
+			"tags":        schemaResourceTags(resourceName),
+		},
+
+		DeprecationMessage: "sakuracloud_enhanced_db is an experimental resource. Please note that you will need to update the tfstate manually if the resource schema is changed.",
+	}
+}
+
+func resourceSakuraCloudEnhancedDBCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client, _, err := sakuraCloudClient(d, meta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	builder := expandEnhancedDBBuilder(d, client, "")
+	created, err := builder.Build(ctx)
+	if created != nil {
+		d.SetId(created.ID.String())
+	}
+	if err != nil {
+		return diag.Errorf("creating SakuraCloud EnhancedDB is failed: %s", err)
+	}
+
+	return resourceSakuraCloudEnhancedDBRead(ctx, d, meta)
+}
+
+func resourceSakuraCloudEnhancedDBRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client, _, err := sakuraCloudClient(d, meta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	regOp := sacloud.NewEnhancedDBOp(client)
+	reg, err := regOp.Read(ctx, sakuraCloudID(d.Id()))
+	if err != nil {
+		if sacloud.IsNotFoundError(err) {
+			d.SetId("")
+			return nil
+		}
+		return diag.Errorf("could not find SakuraCloud EnhancedDB[%s]: %s", d.Id(), err)
+	}
+	return setEnhancedDBResourceData(ctx, d, client, reg, true)
+}
+
+func resourceSakuraCloudEnhancedDBUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client, _, err := sakuraCloudClient(d, meta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	regOp := sacloud.NewEnhancedDBOp(client)
+	reg, err := regOp.Read(ctx, sakuraCloudID(d.Id()))
+	if err != nil {
+		return diag.Errorf("could not read SakuraCloud EnhancedDB[%s]: %s", d.Id(), err)
+	}
+
+	builder := expandEnhancedDBBuilder(d, client, reg.SettingsHash)
+	if _, err := builder.Build(ctx); err != nil {
+		return diag.Errorf("updating SakuraCloud EnhancedDB[%s] is failed: %s", d.Id(), err)
+	}
+
+	return resourceSakuraCloudEnhancedDBRead(ctx, d, meta)
+}
+
+func resourceSakuraCloudEnhancedDBDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client, _, err := sakuraCloudClient(d, meta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	regOp := sacloud.NewEnhancedDBOp(client)
+	reg, err := regOp.Read(ctx, sakuraCloudID(d.Id()))
+	if err != nil {
+		if sacloud.IsNotFoundError(err) {
+			d.SetId("")
+			return nil
+		}
+		return diag.Errorf("could not read SakuraCloud EnhancedDB[%s]: %s", d.Id(), err)
+	}
+
+	if err := regOp.Delete(ctx, reg.ID); err != nil {
+		return diag.Errorf("deleting SakuraCloud EnhancedDB[%s] is failed: %s", d.Id(), err)
+	}
+
+	d.SetId("")
+	return nil
+}
+
+func setEnhancedDBResourceData(ctx context.Context, d *schema.ResourceData, client *APIClient, data *sacloud.EnhancedDB, includePassword bool) diag.Diagnostics {
+	d.Set("name", data.Name)               // nolint
+	d.Set("icon_id", data.IconID.String()) // nolint
+	d.Set("description", data.Description) // nolint
+
+	d.Set("database_type", data.DatabaseType)     // nolint
+	d.Set("database_name", data.DatabaseName)     // nolint
+	d.Set("region", data.Region)                  // nolint
+	d.Set("host_name", data.HostName)             // nolint
+	d.Set("max_connections", data.MaxConnections) // nolint
+
+	return diag.FromErr(d.Set("tags", flattenTags(data.Tags)))
+}
