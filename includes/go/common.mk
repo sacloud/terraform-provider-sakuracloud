19ccbda1da0467d927721392a4a9c78a4857cf90
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2023-12-12 11:00:42 +0900

    golangci-lint v1.55.2

--- includes/go/common.mk
+++ includes/go/common.mk
@@ -19,7 +19,7 @@ COPYRIGHT_YEAR          ?= 2022
 COPYRIGHT_FILES         ?= $$(find . -name "*.go" -print | grep -v "/vendor/")
 GO                      ?= go
 DEFAULT_GOALS           ?= fmt set-license go-licenses-check goimports lint test
-GOLANG_CI_LINT_VERSION  ?= v1.51.2
+GOLANG_CI_LINT_VERSION  ?= v1.55.2
 TEXTLINT_ACTION_VERSION ?= v0.0.3
 
 .DEFAULT_GOAL = default
