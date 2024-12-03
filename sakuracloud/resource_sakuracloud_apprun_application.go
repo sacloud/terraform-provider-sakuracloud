1e14d5c37ca4b80dc9a32051cdb1ff48bd8c0d3b
hekki <contact@hekki.info>
2024-12-03 17:06:43 +0900

    components.prove.http_getをrequiredに変更

--- sakuracloud/resource_sakuracloud_apprun_application.go
+++ sakuracloud/resource_sakuracloud_apprun_application.go
@@ -147,7 +147,7 @@ func resourceSakuraCloudApprunApplication() *schema.Resource {
 								Schema: map[string]*schema.Schema{
 									"http_get": {
 										Type:     schema.TypeList,
-										Optional: true,
+										Required: true,
 										MaxItems: 1,
 										Elem: &schema.Resource{
 											Schema: map[string]*schema.Schema{
