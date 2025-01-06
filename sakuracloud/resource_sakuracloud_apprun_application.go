43b5cf446df68cf5146555af2c310302ba871fc6
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2025-01-06 10:13:38 +0900

    sakuracloud_apprun_application: added status and public_url

--- sakuracloud/resource_sakuracloud_apprun_application.go
+++ sakuracloud/resource_sakuracloud_apprun_application.go
@@ -22,7 +22,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-
 	"github.com/sacloud/apprun-api-go"
 	v1 "github.com/sacloud/apprun-api-go/apis/v1"
 	"github.com/sacloud/terraform-provider-sakuracloud/internal/desc"
@@ -235,6 +234,16 @@ func resourceSakuraCloudApprunApplication() *schema.Resource {
 					},
 				},
 			},
+			"status": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The application status",
+			},
+			"public_url": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The public URL",
+			},
 		},
 	}
 }
@@ -397,6 +406,8 @@ func setApprunApplicationResourceData(d *schema.ResourceData, application *v1.Ap
 	d.Set("max_scale", application.MaxScale)
 	d.Set("components", flattenApprunApplicationComponents(d, application, true))
 	d.Set("traffics", flattenApprunApplicationTraffics(traffics, versions))
+	d.Set("status", *application.Status)
+	d.Set("public_url", *application.PublicUrl)
 
 	return nil
 }
