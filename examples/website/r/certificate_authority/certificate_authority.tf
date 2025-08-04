dc168b71936b1e2446e5011abb9acb033b97f247
Masahiro Nakagawa <repeatedly@gmail.com>
2025-08-04 14:02:30 +0900

    v2.29.0
    Signed-off-by: Masahiro Nakagawa <repeatedly@gmail.com>

--- examples/website/r/certificate_authority/certificate_authority.tf
+++ examples/website/r/certificate_authority/certificate_authority.tf
@@ -6,7 +6,7 @@
 #    }
 #    sakuracloud = {
 #      source  = "sacloud/sakuracloud"
-#      version = "2.28.1"
+#      version = "2.29.0"
 #    }
 #  }
 #}
--- version/version.go
+++ version/version.go
@@ -18,7 +18,7 @@ import "fmt"
 
 var (
 	// Version app version
-	Version = "2.28.1"
+	Version = "2.29.0"
 	// Revision git commit short commithash
 	Revision = "xxxxxx" // set on build
 )
